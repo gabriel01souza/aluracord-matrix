34bdf7a99facc054777b5ec63891ee6930915d89
Gabriel Cardoso de Souza <gabrielcardososouza3007@gmail.com>
2022-01-30 22:40:58 -0300

    Fim imersão React Js + Aluracord atualização

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
 import {Box, Button, Text, TextField, Image} from '@skynexui/components'; 
-
+import react from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Title(props){
   console.log({props});
@@ -64,16 +36,18 @@ function Title(props){
   //export default HomePage
 
   export default function PaginaInicial() {
-    const username = 'gabriel01souza';
-  
+   // const username = 'gabriel01souza';
+  const [username, setUsername] = react.useState('gabriel01souza');
+  const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            // backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+             backgroundImage: 'url(https://img.freepik.com/fotos-gratis/folhas-de-palmeira-tropical-verde-escura-isoladas-em-fundo-gradiente-rosa-roxo-design-para-cartoes-de-convite-folhetos-modelos-de-design-abstrato-para-cartazes-capas-papeis-de-parede-com-copyspace-para-texto_489646-28.jpg?size=626&ext=jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -95,17 +69,45 @@ function Title(props){
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                console.log('Alguém submeteu o fomr');
+                // roteamento.push('/chat' + username);
+                roteamento.push(`/chat?username=${username}`);
+                // window.location.href = '/chat';
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
-              <Title tag="h2">Boas vindas de volta!</Title>
+              <Title tag="h2">Bem vindo ao Aluracord!</Title>
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+              
+              {/* <input 
+                type={"text"}
+                value={username}
+                onChange={function (event){
+                    console.log('usuario digitou', event.target.value)
+                    // Onde ta o valor?
+                    const valor = event.target.value;
+                    // Trocar o valor da variavel
+                    // através do react e avise quem precisa
+                    setUsername(valor);
+                }}
+              /> */}
               <TextField
+                value={username}
+                onChange={function (event){
+                  console.log('usuario digitou', event.target.value)
+                  // Onde ta o valor?
+                  const valor = event.target.value;
+                  // Trocar o valor da variavel
+                  // através do react e avise quem precisa
+                  setUsername(valor);
+              }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -118,7 +120,7 @@ function Title(props){
               />
               <Button
                 type='submit'
-                label='Entrar'
+                label='Login'
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -159,7 +161,7 @@ function Title(props){
                 styleSheet={{
                   color: appConfig.theme.colors.neutrals[200],
                   backgroundColor: appConfig.theme.colors.neutrals[900],
-                  padding: '3px 10px',
+                  padding: '10px 10px',
                   borderRadius: '1000px'
                 }}
               >
